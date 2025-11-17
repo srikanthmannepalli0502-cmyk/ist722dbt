33dba62ec08a5e13cf0d475fbf7142439909fe6c
Srikanth Mannepalli <smannepa@syr.edu>
2025-11-17 01:26:59 +0000

    handling null values

--- quickstart/models/example/my_first_dbt_model.sql
+++ quickstart/models/example/my_first_dbt_model.sql
@@ -12,16 +12,16 @@
 with source_data as (
 
     select 1 as id
-    union all
-    select null as id
+
 
 )
 
 select *
 from source_data
 
+
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+ where id is not null
