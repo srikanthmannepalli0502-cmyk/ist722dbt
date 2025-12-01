64d827f47b8635d519663d42e0991d69f5e6539f
Srikanth Mannepalli <smannepa@syr.edu>
2025-12-01 19:40:27 +0000

    fact trip

--- models/fact/fact_trip.sql
+++ models/fact/fact_trip.sql
@@ -2,61 +2,73 @@
 
 WITH stg AS (
     SELECT *
-    FROM UBER_DW.DW.STG_TRIP
+    FROM UBER_DW.DW.stg_trip
 ),
 
 dim_date AS (
     SELECT *
-    FROM UBER_DW.DW.DIM_DATE
+    FROM UBER_DW.DW.dim_date
 ),
 
 dim_location AS (
     SELECT *
-    FROM UBER_DW.DW.DIM_LOCATION
+    FROM UBER_DW.DW.dim_location
 ),
 
 dim_payment AS (
     SELECT *
-    FROM UBER_DW.DW.DIM_PAYMENT
+    FROM UBER_DW.DW.dim_payment
 ),
 
 dim_rider AS (
     SELECT *
-    FROM UBER_DW.DW.DIM_RIDER
+    FROM UBER_DW.DW.dim_rider
 ),
 
 dim_driver AS (
     SELECT *
-    FROM UBER_DW.DW.DIM_DRIVER
+    FROM UBER_DW.DW.dim_driver
 )
 
 SELECT
     d1.date_id AS pickup_date_id,
     d2.date_id AS dropoff_date_id,
+
     r.rider_id,
     dr.driver_id,
+
     l1.location_id AS pickup_location_id,
     l2.location_id AS dropoff_location_id,
+
     p.payment_id,
+
     stg.fare_amount,
-    stg.trip_distance,
     stg.passenger_count,
+
+    -- Trip duration in minutes
     DATEDIFF('minute', stg.pickup_datetime, stg.dropoff_datetime) AS trip_duration_min
 
 FROM stg
+
 LEFT JOIN dim_date d1
     ON d1.full_date = CAST(stg.pickup_datetime AS DATE)
+
 LEFT JOIN dim_date d2
     ON d2.full_date = CAST(stg.dropoff_datetime AS DATE)
+
 LEFT JOIN dim_location l1
     ON l1.latitude  = stg.pickup_latitude
    AND l1.longitude = stg.pickup_longitude
+
 LEFT JOIN dim_location l2
     ON l2.latitude  = stg.dropoff_latitude
    AND l2.longitude = stg.dropoff_longitude
+
 LEFT JOIN dim_payment p
     ON p.payment_type = stg.payment_type
+
 LEFT JOIN dim_rider r
     ON r.rider_id = (ABS(MOD(HASH(stg.pickup_datetime), 1480000)) + 1)
+
 LEFT JOIN dim_driver dr
     ON dr.driver_id = (ABS(MOD(HASH(stg.dropoff_datetime), 74000)) + 1)
